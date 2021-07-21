6fbaccfa011b394354ed557953ce1af8a56d7f94
Lucy <lucybellott@gmail.com>
2021-07-21 18:09:20 -0400

    commit tree fc6587b1fb9dc199b6ec1b2179bfee03648fd6cb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    
+    # validate :clickbait
+
+    
+  def clickbait
+    clickbait_examples = [ "Won't Believe","Secret","Top","Guess" ]
+   clickbait = clickbait_examples.any? {|word| word.match title}
+   if !clickbait
+    errors.add(:title, "it's not clickbait")
+   end
+  end
+
+
 end
